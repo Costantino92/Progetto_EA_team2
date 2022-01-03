9ace15c16b5f3a9dec83333f79ca7998dad44e66
Costantino92 <dragos.ctin92@gmail.com>
2022-01-03 16:35:53 +0100

    Design System FinutuFinutu

--- design-system/card.js
+++ design-system/card.js
@@ -1,23 +1,20 @@
-const battlefieldCard = document.querySelector('.battleFieldCard');
+const battlefieldCard = document.querySelector(".battleFieldCard");
 
-const battlefieldLogo= document.querySelector('.battleFieldLogo');
-const gradientColor = document.querySelector('.gradientColor');
+const battlefieldLogo = document.querySelector(".battleFieldLogo");
+const gradientColor = document.querySelector(".gradientColor");
 
-function animCard(){
-    
-    battlefieldLogo.style.transform = 'translateY(-60px)';
-    battlefieldLogo.style.transition = 'all 300ms ease-out;';
-    gradientColor.style.display='flex';
-    gradientColor.style.color='white';
+function animCard() {
+  battlefieldLogo.style.transform = "translateY(-60px)";
+  battlefieldLogo.style.transition = "all 300ms ease-out;";
+  gradientColor.style.opacity = "1";
+  gradientColor.style.color = "white";
 }
 
-function disAnimCard(){
-    
-    battlefieldLogo.style.transform = 'translateY(0)';
-    battlefieldLogo.style.transition = 'all 300ms ease-out;';
-    gradientColor.style.display='none';
+function disAnimCard() {
+  battlefieldLogo.style.transform = "translateY(0)";
+  battlefieldLogo.style.transition = "all 300ms ease-out;";
+  gradientColor.style.opacity = "0";
 }
 
-
-battlefieldCard.addEventListener('mouseover',animCard);
-battlefieldCard.addEventListener('mouseout',disAnimCard);
+battlefieldCard.addEventListener("mouseover", animCard);
+battlefieldCard.addEventListener("mouseout", disAnimCard);
